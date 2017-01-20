14a95d09e9fe0e9723af109e3b3b4d2244d919b6
Ilyes Hammadi <hammadiilyesahmed@gmail.com>
2017-01-20 13:28:53 +0100

    update About

--- src/js/About.js
+++ src/js/About.js
@@ -1,10 +1,12 @@
 import React from 'react'
 
+import '../css/About.css'
+
 class About extends React.Component {
   render() {
     return (
-      <div>
-        <h1>About !!</h1>
+      <div id="dev">
+        <h1>Developed with 	&#9829; by <a href="https://github.com/Ilyes-Hammadi">Ilyes Hammadi</a></h1>
       </div>
     )
   }
